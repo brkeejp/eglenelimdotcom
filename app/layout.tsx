5d9c92e1b67dcac28c9848a33ac44f92aeeea4a8
Berk Ege <brkee.jp@gmail.com>
2023-12-21 03:08:26 +0300

    another test push

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,46 +20,82 @@ export default function RootLayout({
       <body className={inter.className}>
         <header>
           <a
-            href='/'
-            >
-              <img src='' />
-            </a>
+            href="/"
+          >
+            <Image
+              src="/eglenelim.svg"
+              alt="Eğlenelim Logo"
+              width={100}
+              height={25}
+              priority
+            />
+          </a>
         </header>
         {children}
         <footer>
-          <a
-            className=""
-            href="https://nextjs.org?utm_source=eglenelimdotcom&utm_medium=poweredbyheader"
-            target="_blank"
-            rel="noopener noreferrer"
+          <div>
+            <div>
+              <Image
+                src="/eglenelim.svg"
+                alt="Eğlenelim Logo"
+                width={150}
+                height={50}
+                priority
+              />
+            </div>
+            <div>
+              <p>Social</p>
+              <a>Twitter ( X )</a>
+              <a>Facebook</a>
+              <a>Instagram</a>
+              <a>Youtube</a>
+            </div>
+            <div>
+              <p>Company</p>
+              <a>About Us</a>
+              <a>Team</a>
+            </div>
+            <div>
+              <p>Community</p>
+              <a>Join Us</a>
+              <a>Blog</a>
+            </div>
+          </div>
+          <div>
+            <a
+              className=""
+              href="https://nextjs.org?utm_source=eglenelimdotcom&utm_medium=poweredbyheader"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-            Powered by 
-            <Image
-              src="/next.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-          <span className='VSperator' />
-          <a
-            className=""
-            href="https://brkee.net?utm_source=eglenelimdotcom&utm_medium=designedbyheader"
-            target="_blank"
-            rel="noopener noreferrer"
+              Powered by
+              <Image
+                src="/next.svg"
+                alt="Vercel Logo"
+                className="dark:invert"
+                width={72}
+                height={10}
+                priority
+              />
+            </a>
+            <span className='VSperator' />
+            <a
+              className=""
+              href="https://brkee.net?utm_source=eglenelimdotcom&utm_medium=designedbyheader"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-            Designed by
-            <Image
-              src="/brkee_wide.svg"
-              alt="brkee Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+              Designed by
+              <Image
+                src="/brkee_wide.svg"
+                alt="brkee Logo"
+                className="dark:invert"
+                width={72}
+                height={10}
+                priority
+              />
+            </a>
+          </div>
         </footer>
       </body>
     </html>
